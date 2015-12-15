b111145f538716a518a9f4b1e1b00be127998466
Fekete Krisztián <fekete.krisztyan@gmail.com>
2015-12-15 22:24:10 +0100

    docstring for html module

--- sablon/html.py
+++ sablon/html.py
@@ -1,3 +1,7 @@
+'''
+html templating is an extra, it is not the main goal.
+Consider using `YAFOWIL`/`nontemplate`/`yattag`/`temper` instead.
+'''
 from __future__ import unicode_literals
 from __future__ import print_function
 from __future__ import absolute_import
